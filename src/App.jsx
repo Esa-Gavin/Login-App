40f2c619c4ecb4a24bb0c3f062f35d785bd19dfe
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-01-11 15:41:29 +0300

    create use states in the relevant components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import MainHeader from './components/MainHeader/MainHeader';
 
 const App = () => {
@@ -18,8 +21,11 @@ const App = () => {
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
-        {!isLoggedIn}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
     </React.Fragment>
-  )
+  );
 }
+
+export default App;
---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Navigation.module.css';
+
+const Navigation = () => {
+    return
+};
+
+export default Navigation;
