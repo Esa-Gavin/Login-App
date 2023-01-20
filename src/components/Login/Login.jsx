e0eb40394ba398e4dd1e3d185ac950d4e4f85972
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-01-20 17:51:47 +0300

    create a function to target the value entered by the user using setEnteredEmail

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,13 @@ import classes from './Login.module.css';
 const Login = (props) => {
     const [enteredEmail, setEnteredEmail]  = useState('');
     const [emailIsValid, setEmailIsValid] = useState();
+    const [formIsValid, setFormIsValid] = useState(false);
+    
+    
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    }
+    
     const submitHandler = (event) => {
         event.preventDefault();
         props.onLogin(enteredEmail);
