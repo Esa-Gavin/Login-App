d2658545e1f11e1ac3f8f896d4f0198be16cf964
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-01-20 17:47:07 +0300

    add a use state for validating an email

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,7 +4,7 @@ import classes from './Login.module.css';
 
 const Login = (props) => {
     const [enteredEmail, setEnteredEmail]  = useState('');
-
+    const [emailIsValid, setEmailIsValid] = useState();
     const submitHandler = (event) => {
         event.preventDefault();
         props.onLogin(enteredEmail);
@@ -13,7 +13,9 @@ const Login = (props) => {
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                
+                <div className=''>
+
+                </div>
             </form>
 
         </Card>
